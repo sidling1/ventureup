732077a379a41bf709d1608f8739188f0cb093d2
sidjethwani@gmail.com <sidjethwani@gmail.com>
2024-06-13 22:23:45 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React , { Fragment } from "react";
+import Init from "./Components/Init";
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Init/>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Init.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import  {Fragment, useEffect, useState} from "react";
+
+function Init() {
+    const [data, setData] = useState("empty");
+
+    useEffect(() => {
+        fetch('http://localhost:3050/api/')
+            .then(
+            res => res.text()
+            ).then(
+                data => setData(data)
+            );
+    }, []);
+
+    return (
+        <Fragment>
+            {data}
+        </Fragment>
+    );
+}
+
+export default Init;
